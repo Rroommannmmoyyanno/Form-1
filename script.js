4f443e86de8c2317d0022b6a9e250954eb3f09c1
Rroommannmmoyyanno <romanmoyano14@gmail.com>
2025-04-03 19:26:35 -0300

    Optimización del diseño y restauración de materias en el archivo index.html

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Configuración de Flatpickr para el campo de fecha
-  flatpickr("#fecha", {
-    dateFormat: "Y-m-d", // Formato de fecha (YYYY-MM-DD)
-    minDate: "today", // Deshabilita fechas pasadas
-    maxDate: new Date().fp_incr(547), // Permite seleccionar hasta 1 año y medio (547 días)
-    locale: "es", // Configuración de idioma en español
-    altInput: true, // Muestra un campo alternativo con formato legible
-    altFormat: "d-m-Y", // Formato alternativo (DD-MM-YYYY)
-  });
-
   // Configuración de Select2 para el campo de materias
   if (typeof $ !== "undefined" && $.fn.select2) {
     $("#materia").select2({
